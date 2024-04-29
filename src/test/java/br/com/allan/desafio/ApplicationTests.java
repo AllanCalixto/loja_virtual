f57f9a7d3f5b132e74d7215f25f5907cf7e8a686
Allan <allancalixto1@gmail.com>
2024-04-29 14:25:23 -0300

    commit inicial do projeto

---
+++ src/test/java/br/com/allan/desafio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allan.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
