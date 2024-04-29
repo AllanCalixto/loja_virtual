66eaa140ec46b6fc7b1c4eb55b0c6ca6852d1b4f
Allan <allancalixto1@gmail.com>
2024-04-29 17:58:19 -0300

    implementacao classes pessoa, pessoa fisica e pessoa juridica

--- src/main/java/br/com/solvetech/loja_virtual/model/Acesso.java
+++ src/main/java/br/com/solvetech/loja_virtual/model/Acesso.java
@@ -1,6 +1,5 @@
 package br.com.solvetech.loja_virtual.model;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -11,19 +10,25 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-@Entity
-@Table(name= "categoria_produto")
-@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
-public class CategoriaProduto implements Serializable{
+import org.springframework.security.core.GrantedAuthority;
 
-	private static final long serialVersionUID = 1L;
-	
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
 	private Long id;
-	
-	@Column(name = "nome_desc", nullable = false)
-	private String nomeDesc;
+
+	@Column(nullable = false)
+	private String descricao; /* Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO */
+
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return this.descricao;
+	}
 
 	public Long getId() {
 		return id;
@@ -33,12 +38,12 @@ public class CategoriaProduto implements Serializable{
 		this.id = id;
 	}
 
-	public String getNomeDesc() {
-		return nomeDesc;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setNomeDesc(String nomeDesc) {
-		this.nomeDesc = nomeDesc;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 	@Override
@@ -54,12 +59,8 @@ public class CategoriaProduto implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		CategoriaProduto other = (CategoriaProduto) obj;
+		Acesso other = (Acesso) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
 
 }
