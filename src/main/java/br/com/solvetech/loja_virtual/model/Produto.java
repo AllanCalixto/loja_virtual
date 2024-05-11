c95e16e894a96f323fa057a73c075195cae8348f
Allan <allancalixto1@gmail.com>
2024-05-11 19:44:38 -0300

    ajustes na implementacao das obrigatoriedades das classes

--- src/main/java/br/com/solvetech/loja_virtual/model/Produto.java
+++ src/main/java/br/com/solvetech/loja_virtual/model/Produto.java
@@ -23,29 +23,39 @@ public class Produto implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
 	private Long id;
 
+	@Column(nullable = false)
 	private String tipoUnidade;
 
+	@Column(nullable = false)
 	private String nome;
 	
+	@Column(nullable = false)
 	private Boolean ativo = Boolean.TRUE;
 
-	@Column(columnDefinition = "text", length = 2000)
+	@Column(columnDefinition = "text", length = 2000, nullable = false)
 	private String descricao;
 
 	/** Nota item produto - ASSOCIAR **/
 
+	@Column(nullable = false)
 	private Double peso;
 
+	@Column(nullable = false)
 	private Double largura;
 
+	@Column(nullable = false)
 	private Double altura;
 
+	@Column(nullable = false)
 	private Double profundidade;
 
+	@Column(nullable = false)
 	private BigDecimal valorVenda = BigDecimal.ZERO;
 
+	@Column(nullable = false)
 	private Integer QtdeEstoque = 0;
 
+	@Column(nullable = false)
 	private Integer QtdeAlertaEstoque = 0;
 
 	private String linkYoutube;
