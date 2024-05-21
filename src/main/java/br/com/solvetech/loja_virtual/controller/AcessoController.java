3c9576866f4ac4171eea2d6e24f9487c7293448b
Allan <allancalixto1@gmail.com>
2024-05-20 21:30:06 -0300

    criacao de endpoints

--- src/main/java/br/com/solvetech/loja_virtual/controller/AcessoController.java
+++ src/main/java/br/com/solvetech/loja_virtual/controller/AcessoController.java
@@ -1,9 +1,16 @@
 package br.com.solvetech.loja_virtual.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.solvetech.loja_virtual.model.Acesso;
+import br.com.solvetech.loja_virtual.repository.AcessoRepository;
 import br.com.solvetech.loja_virtual.service.AcessoService;
 
 @RestController
@@ -12,8 +19,22 @@ public class AcessoController {
 	@Autowired
 	private AcessoService acessoService;
 	
-	public Acesso salvarAcesso(Acesso acesso) {
-		return acessoService.save(acesso);
+	@Autowired
+	private AcessoRepository acessoRepository;
+	
+	@PostMapping("**/salvarAcesso")
+	@ResponseBody
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
+		Acesso acessoSalvo = acessoService.save(acesso);
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+		
+	}
+	
+	@DeleteMapping("**/deleteAcesso")
+	@ResponseBody
+	public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso) {
+		acessoRepository.deleteById(acesso.getId());
+		return new ResponseEntity("Acesso removido com sucesso!", HttpStatus.OK);
 	}
 
 }
