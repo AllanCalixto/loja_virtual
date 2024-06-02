3cac53893452ed7c8e3a46087d8ac7ad2861725b
Allan <allancalixto1@gmail.com>
2024-06-02 00:18:31 -0300

    configuracao inicial flyway

--- src/test/java/br/com/solvetech/LojaVirtualApplicationTests.java
+++ src/test/java/br/com/solvetech/LojaVirtualApplicationTests.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -24,6 +25,7 @@ import br.com.solvetech.loja_virtual.model.Acesso;
 import br.com.solvetech.loja_virtual.repository.AcessoRepository;
 import junit.framework.TestCase;
 
+@Profile("dev")
 @SpringBootTest(classes = Application.class)
 class LojaVirtualApplicationTests extends TestCase{
 	
