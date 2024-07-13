17dd0a7192ce359e4e50b7b7ca9185522ac7b044
Allan <allancalixto1@gmail.com>
2024-07-13 19:04:31 -0300

    JWTLoginFilter

--- src/main/java/br/com/solvetech/loja_virtual/controller/AcessoController.java
+++ src/main/java/br/com/solvetech/loja_virtual/controller/AcessoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,6 +44,8 @@ public class AcessoController {
 	}
 	
 
+	//@CrossOrigin(origins = "https://www.jdevtreinamentos.com.br")
+	//@Secured({ "ROLE_GERENTE", "ROLE_ADMIN" })
 	@DeleteMapping("**/deleteAcessoPorId/{id}")
 	@ResponseBody
 	public ResponseEntity<?> deleteAcessoPorId(@PathVariable("id") Long id) {
--- src/main/java/br/com/solvetech/loja_virtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/br/com/solvetech/loja_virtual/security/JWTTokenAutenticacaoService.java
@@ -2,11 +2,17 @@ package br.com.solvetech.loja_virtual.security;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import br.com.solvetech.loja_virtual.ApplicationContextLoad;
+import br.com.solvetech.loja_virtual.model.Usuario;
+import br.com.solvetech.loja_virtual.repository.UsuarioRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -43,8 +49,60 @@ public class JWTTokenAutenticacaoService {
 		/* Dá a resposta pra tela e para o cliente */
 		response.addHeader(HEADER_STRING, token);
 		
+		liberacaoCors(response);
+		
 		/* Usado para ver no Postman para teste */
 		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+	}
+	
+	/* Retorna o usuário validado com token ou caso nao seja valido retorna null */
+	public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) {
+		
+		String token = request.getHeader(HEADER_STRING);
+		if(token != null) {
+			String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+			
+			/* Faz a validação do token do usuário na requisicao e obtem o USER */
+			String user =  Jwts.parser()
+					.setSigningKey(SECRET)
+					.parseClaimsJws(tokenLimpo)
+					.getBody().getSubject(); /* ADMIN ou Alex*/
+			
+			
+			if(user != null) {
+				Usuario usuario = ApplicationContextLoad.
+						getApplicationContext().
+						getBean(UsuarioRepository.class).findUserByLogin(user);
+				
+				if(usuario != null) {
+					return new UsernamePasswordAuthenticationToken(
+							usuario.getLogin(),
+							usuario.getSenha(),
+							usuario.getAuthorities());
+				}
+			}
+		}
+		
+		
+		liberacaoCors(response);
+		return null;
+	}
+	
+	/* Fazendo liberação contra erro de Cors no navegador */
+	private void liberacaoCors(HttpServletResponse response) {
+		
+		if(response.getHeader("Acess-Control-Allow-Origin") == null) {
+			response.addHeader("Acess-Control-Allow-Origin", "*");
+		}
+		if(response.getHeader("Acess-Control-Allow-Headers") == null) {
+			response.addHeader("Acess-Control-Allow-Headers", "*");
+		}
+		if(response.getHeader("Acess-Control-Request-Headers") == null) {
+			response.addHeader("Acess-Control-Request-Headers", "*");
+		}
+		if(response.getHeader("Acess-Control-Allow-Methods") == null) {
+			response.addHeader("Acess-Control-Allow-Methods", "*");
+		}
 	}
 	
 }
